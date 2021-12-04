989842dafe07fa734a8d30413046479d8664b53d
Richard Lim <hello@rclim95.com>
2021-12-04 13:46:05 -0600

    Convert existing solutions for 2021 to use advent2021.core.run format
    Make it consistent with Day 4, which is the "runner" format I plan to
    use for the remainder of Advent of Code 2021 (for as long as I continue
    working on these puzzles. :sweat_smile:)

--- 2021/advent2021/day3/__main__.py
+++ 2021/advent2021/day3/__main__.py
@@ -1,71 +1,68 @@
 import itertools
 import os
-import sys
+from advent2021.core import run
 
 SCRIPT_DIR = os.path.dirname(__file__)
 INPUT_FILE = "input.txt"
 
-def part1():
+def part1(input_fp):
     # Read the input file
-    with open(os.path.join(SCRIPT_DIR, INPUT_FILE), "r") as input_fp:
-        zeroes_count = []
-        ones_count = []
+    zeroes_count = []
+    ones_count = []
 
-        # Go through each line (which should represent a binary number)
-        for line in input_fp:
-            # Split our line by its individual bit
-            bits = list(line.strip())
+    # Go through each line (which should represent a binary number)
+    for line in input_fp:
+        # Split our line by its individual bit
+        bits = list(line.strip())
 
-            # Are our count empty? If so, use the first line to figure out how many bits we're
-            # working with. :)
-            if len(zeroes_count) == 0 and len(ones_count) == 0:
-                zeroes_count.extend(itertools.repeat(0, len(bits)))
-                ones_count.extend(itertools.repeat(0, len(bits)))
+        # Are our count empty? If so, use the first line to figure out how many bits we're
+        # working with. :)
+        if len(zeroes_count) == 0 and len(ones_count) == 0:
+            zeroes_count.extend(itertools.repeat(0, len(bits)))
+            ones_count.extend(itertools.repeat(0, len(bits)))
 
-            # Go through each bit and increment the proper count
-            for (i, bit) in enumerate(bits):
-                if int(bit) == 0:
-                    zeroes_count[i] += 1
-                elif int(bit) == 1:
-                    ones_count[i] += 1
+        # Go through each bit and increment the proper count
+        for (i, bit) in enumerate(bits):
+            if int(bit) == 0:
+                zeroes_count[i] += 1
+            elif int(bit) == 1:
+                ones_count[i] += 1
 
-        # Number crunching time!
-        #
-        # Figure out the gamma rate by seeing which is the most common bit for each "bit position",
-        # (e.g., if 1 appeared more than 0 for the first bit, we put a "1"; if 0 appeared more than 1,
-        # then we put a "0", next; so far, our binary number would then be "10" in Base2, or 2 in Base10).
-        #
-        # For the epsilon rate, the above is still the same, except we flip it.
-        gamma_rate = 0
-        epsilon_rate = 0
-        for (lshift_count, (zero_count, one_count)) in enumerate(zip(zeroes_count, ones_count)):
-            if zero_count > one_count:
-                # More zeroes were found than one.
-                gamma_rate |= 0 << (len(bits) - lshift_count - 1)
-                epsilon_rate |= 1 << (len(bits) - lshift_count - 1)
-            else:
-                # More ones were found than zeroes.
-                gamma_rate |= 1 << (len(bits) - lshift_count - 1)
-                epsilon_rate |= 0 << (len(bits) - lshift_count - 1)
+    # Number crunching time!
+    #
+    # Figure out the gamma rate by seeing which is the most common bit for each "bit position",
+    # (e.g., if 1 appeared more than 0 for the first bit, we put a "1"; if 0 appeared more than 1,
+    # then we put a "0", next; so far, our binary number would then be "10" in Base2, or 2 in Base10).
+    #
+    # For the epsilon rate, the above is still the same, except we flip it.
+    gamma_rate = 0
+    epsilon_rate = 0
+    for (lshift_count, (zero_count, one_count)) in enumerate(zip(zeroes_count, ones_count)):
+        if zero_count > one_count:
+            # More zeroes were found than one.
+            gamma_rate |= 0 << (len(bits) - lshift_count - 1)
+            epsilon_rate |= 1 << (len(bits) - lshift_count - 1)
+        else:
+            # More ones were found than zeroes.
+            gamma_rate |= 1 << (len(bits) - lshift_count - 1)
+            epsilon_rate |= 0 << (len(bits) - lshift_count - 1)
 
-        # Now get our result--the power consumption (multiplication of gamma and epsilon)
-        print(gamma_rate * epsilon_rate)
+    # Now get our result--the power consumption (multiplication of gamma and epsilon)
+    return gamma_rate * epsilon_rate
 
 
-def part2():
+def part2(input_fp):
     numbers = []
 
-    # Read the input file
-    with open(os.path.join(SCRIPT_DIR, INPUT_FILE), "r") as input_fp:
-        # We're going to need to do several pasts with the file, so let's load all the binary
-        # numbers into memory (have a list of tuple of bits).
-        #
-        # While we're add it, figure out the number of zeroes and ones for each bit position. This
-        # will help us determine oxygen generator rating and the CO2 scrubber rating (as these ratings
-        # are dependent on the "frequency" of 0's and 1's in each position)
-        for line in input_fp:
-            bits_list = list(line.strip())
-            numbers.append(tuple(int(bit) for bit in bits_list))
+    # We're going to need to do several pasts with the file, so let's load all the binary
+    # numbers into memory (have a list of tuple of bits).
+    #
+    # While we're add it, figure out the number of zeroes and ones for each bit position. This
+    # will help us determine oxygen generator rating and the CO2 scrubber rating (as these ratings
+    # are dependent on the "frequency" of 0's and 1's in each position)
+    for line in input_fp:
+        bits_list = list(line.strip())
+        numbers.append(tuple(int(bit) for bit in bits_list))
 
     # How many bits are we working with? We're going to need this information later. :)
     bit_count = len(numbers[0])
@@ -120,7 +117,7 @@ def part2():
             break
 
     # Finally, figure out the life support rating of the submarine by multiplying those two numbers.
-    print(__bin2dec(co2_scrubber_rating) * __bin2dec(oxygen_generator_rating_binary))
+    return __bin2dec(co2_scrubber_rating) * __bin2dec(oxygen_generator_rating_binary)
 
 def __bin2dec(bits):
     number = 0
@@ -128,9 +125,4 @@ def __bin2dec(bits):
         number += 2 ** (len(bits) - bit_pos - 1) * bit
     return number
 
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        part2()
-    else:
-        part1()
+run(__package__, part1, part2)
