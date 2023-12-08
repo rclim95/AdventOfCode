a75ec0baeb78907077c312bd233d58125a7fe8be
Richard Lim <hello@rclim95.com>
2023-12-08 13:31:04 -0600

    [2023] Add AdventOfCode.Tests project
    This project will be used for including unit testing for some auxiliary
    classes or methods that are needed for solving a puzzle. :smile:
    
    This makes it easier to test logic and assumptions than having to craft
    a puzzle input and running the debugger (via the `--debug` flag) to
    ensure that the auxiliary class and/or method are working properly.

---
+++ 2023/src/AdventOfCode.Tests/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// <copyright file="GlobalUsings.cs" company="rclim95">
+// Copyright (c) rclim95
+//
+// Licensed under the MIT License. For more info, see the LICENSE.md at the root of this repo.
+// </copyright>
+
+global using Xunit;
