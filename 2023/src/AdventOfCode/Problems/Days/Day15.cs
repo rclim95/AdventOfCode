5a761753eb5b00cf93539e52f0db079923460d11
Richard Lim <hello@rclim95.com>
2023-12-19 01:03:53 -0600

    [2023 - Day 15] Implement Part 2 solution for Day 15
    Use a dictionary to represent each box number and the focal lenses (and
    label) that are within the box, and do the proper operations that's
    needed based on the provided initialization step. 😎
    
    For a Day 15, Part 1 and Part 2 are very simple compared to the previous
    days that I've solved so far. 😅

--- 2023/src/AdventOfCode/Problems/Days/Day15.cs
+++ 2023/src/AdventOfCode/Problems/Days/Day15.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode.Problems.Days.Day15;
@@ -26,30 +27,92 @@ internal class Day15 : IPuzzle
         int runningSum = 0;
         foreach (string step in GetInitializeSequenceSteps(reader))
         {
-            // The conversion process is as follow:
-            // - Determine the ASCII code for the current character.
-            // - Increase the current value by that ASCII code.
-            // - Multiply the current value by 17.
-            // - Divide the current value by 256 and take the modulo (remainder).
-            //
-            // Rinse and repeat for each step. :)
-            int currentValue = 0;
-            foreach (char character in step)
-            {
-                currentValue += (int)character;
-                currentValue *= 17;
-                currentValue %= 256;
-            }
-
-            Console.WriteLine($"{step} = {currentValue}");
-            runningSum += currentValue;
+            runningSum += Hash(step);
         }
 
         return runningSum.ToString();
     }
 
     /// <inheritdoc />
-    public static string GetPart2Answer(TextReader reader) => throw new NotImplementedException();
+    public static string GetPart2Answer(TextReader reader)
+    {
+        // Initialize our boxes. We're expecting a total of 256 boxes to deal with. :)
+        Dictionary<int, List<(string Label, int FocalLength)>> boxesAndLenses = Enumerable.Range(0, 256)
+            .ToDictionary(b => b, b => new List<(string Label, int FocalLength)>());
+
+        foreach (string step in GetInitializeSequenceSteps(reader))
+        {
+            Match match = Regex.Match(step, @"(?'label'\w+)(?'operation'=|-)(?'length'\d)*");
+            string label = match.Groups["label"].Value;
+            string operation = match.Groups["operation"].Value;
+            int box = Hash(label);
+
+            if (operation == "-")
+            {
+                // We are removing a lens with the specified label. Go to the box, look for the lens
+                // with the provided label, and then remove it.
+                List<(string Label, int FocalLength)> lenses = boxesAndLenses[box];
+                int indexOfLabel = lenses.FindIndex(l => l.Label == label);
+                if (indexOfLabel >= 0)
+                {
+                    lenses.RemoveAt(indexOfLabel);
+                }
+            }
+            else if (operation == "=")
+            {
+                // We are adding a lens with the specified label and focal length into the box at the
+                // end. Go to the box, and add it _or_ if the label already exists, replace it!
+                List<(string Label, int FocalLength)> lenses = boxesAndLenses[box];
+                int indexOfLabel = lenses.FindIndex(l => l.Label == label);
+                int length = int.Parse(match.Groups["length"].Value);
+                if (indexOfLabel >= 0)
+                {
+                    lenses[indexOfLabel] = (label, length);
+                }
+                else
+                {
+                    lenses.Add((label, length));
+                }
+            }
+        }
+
+        // Now sum up all the focusing power of each lenses.
+        int totalFocusPower = 0;
+        foreach (KeyValuePair<int, List<(string Label, int FocalLength)>> boxAndLenses in boxesAndLenses)
+        {
+            if (boxAndLenses.Value.Count == 0)
+            {
+                continue;
+            }
+
+            foreach (((string label, int focalLength), int slot) in boxAndLenses.Value.Select((l, i) => (l, i + 1)))
+            {
+                totalFocusPower += (1 + boxAndLenses.Key) * slot * focalLength;
+            }
+        }
+
+        return totalFocusPower.ToString();
+    }
+
+    private static int Hash(string input)
+    {
+        // The conversion process is as follow:
+        // - Determine the ASCII code for the current character.
+        // - Increase the current value by that ASCII code.
+        // - Multiply the current value by 17.
+        // - Divide the current value by 256 and take the modulo (remainder).
+        //
+        // Rinse and repeat for each step. :)
+        int currentValue = 0;
+        foreach (char character in input)
+        {
+            currentValue += (int)character;
+            currentValue *= 17;
+            currentValue %= 256;
+        }
+
+        return currentValue;
+    }
 
     private static IEnumerable<string> GetInitializeSequenceSteps(TextReader reader)
     {
@@ -79,7 +142,7 @@ internal class Day15 : IPuzzle
             }
 
             char currentCharacter = (char)reader.Read();
-            if (currentCharacter != '\n')
+            if (!char.IsWhiteSpace(currentCharacter))
             {
                 sb.Append(currentCharacter);
             }
