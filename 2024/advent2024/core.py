c57e51effce00ed47b81d7093454a3f451db8f4e
Richard Lim <hello@rclim95.com>
2024-12-08 14:28:23 -0600

    [2024] Add some more features to the `advent2024.dayX` runner
    * Passing `--verbose` will enable logging verbose output (by default,
      anything with `DEBUG` level or higher) to stderr. This will be useful
      for debugging.
    
    * If `NotImplementedError` was thrown, handle that (special) case by
      letting the user know that the part hasn't been implemented yet.

--- 2024/advent2024/core.py
+++ 2024/advent2024/core.py
@@ -1,5 +1,8 @@
 import argparse
 from dataclasses import dataclass
+import logging
+from logging import NullHandler, StreamHandler
+import sys
 import typing
 import time
 import traceback
@@ -19,6 +22,9 @@ def run(
     parser.add_argument("infile",
                        type=argparse.FileType("r"),
                        help="The input file to run the solution against.")
+    parser.add_argument("--verbose",
+                        action="store_true",
+                        help="Enable verbose logging.")
     parser.add_argument("--part",
                         type=int,
                         default=1,
@@ -30,40 +36,52 @@ def run(
                         help="The expected result (used for testing to ensure the solution is working properly).")
 
     args = parser.parse_args()
-    with args.infile:
-        if args.part == 2:
-            run_part = part2_func
-        else:
-            run_part = part1_func
 
-        # For funsies, we'll also time out how long it took to invoke the function.
-        started_at = time.time()
-        try:
+    # Create the logger based on the arguments passed via the command line.
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[StreamHandler(sys.stderr) if args.verbose else NullHandler()],
+    )
+
+    if args.part == 2:
+        run_part = part2_func
+    else:
+        run_part = part1_func
+
+    try:
+        with args.infile:
+            # For funsies, we'll also time out how long it took to invoke the function.
+            started_at = time.time()
             result = run_part(args.infile)
-        except Exception as e:
-            result = e
-        ended_at = time.time()
+    except Exception as e:
+        result = e
+    ended_at = time.time()
 
-        # Now print out a summary!
-        duration = ended_at - started_at
-        print()
-        print("The results are in!")
-        if isinstance(result, Exception):
-            print(Fore.RED + Style.DIM + "[❌] Program Encounterd Error")
-            print(Fore.LIGHTBLUE_EX + "[⏱] Duration:\t{0:#.3f} seconds".format(duration))
-            print(Fore.YELLOW + "[❌] Exception: ")
-            traceback.print_exception(type(result), result, result.__traceback__)
+    # Now print out a summary!
+    duration = ended_at - started_at
+    print()
+    print("The results are in!")
+    if isinstance(result, NotImplementedError):
+        print(Fore.RED + Style.BRIGHT + "[❌] Program Not Implemented" + Style.RESET_ALL)
+        print(Fore.LIGHTBLUE_EX + "[⏱] Duration:\t{0:#.3f} seconds".format(duration))
+        print(Fore.YELLOW + "[❌] This part of the program has not been implemented yet.")
+    elif isinstance(result, Exception):
+        print(Fore.RED + Style.DIM + "[❌] Program Encounterd Error")
+        print(Fore.LIGHTBLUE_EX + "[⏱] Duration:\t{0:#.3f} seconds".format(duration))
+        print(Fore.YELLOW + "[❌] Exception: ")
+        traceback.print_exception(type(result), result, result.__traceback__)
+    else:
+        # Did we meet our expected result (if provided)?
+        if args.expected is not None and result == args.expected:
+            print(Fore.GREEN + Style.BRIGHT + "[✅] Solution Ran with Expected Result" + Style.RESET_ALL)
+        elif args.expected is not None:
+            print(Fore.YELLOW + Style.BRIGHT + "[⚠️ ] Solution Ran with Unexpected Result" + Style.RESET_ALL)
         else:
-            # Did we meet our expected result (if provided)?
-            if args.expected is not None and result == args.expected:
-                print(Fore.GREEN + Style.BRIGHT + "[✅] Solution Ran with Expected Result" + Style.RESET_ALL)
-            elif args.expected is not None:
-                print(Fore.YELLOW + Style.BRIGHT + "[⚠️ ] Solution Ran with Unexpected Result" + Style.RESET_ALL)
-            else:
-                print(Fore.GREEN + Style.BRIGHT + "[✅] Solution Ran Successfully" + Style.RESET_ALL)
+            print(Fore.GREEN + Style.BRIGHT + "[✅] Solution Ran Successfully" + Style.RESET_ALL)
 
-            print(Fore.LIGHTBLUE_EX + "[⏱ ] Duration:\t{0:#.3f} seconds".format(duration))
-            print(Fore.BLUE + "[🤔] Result:\t" + Style.DIM + str(result), Style.RESET_ALL)
+        print(Fore.LIGHTBLUE_EX + "[⏱ ] Duration:\t{0:#.3f} seconds".format(duration))
+        print(Fore.BLUE + "[🤔] Result:\t" + Style.DIM + str(result), Style.RESET_ALL)
 
-            if args.expected is not None:
-                print(Fore.LIGHTBLUE_EX + "[👀] Expected:\t" + Style.BRIGHT + str(args.expected), Style.RESET_ALL)
+        if args.expected is not None:
+            print(Fore.LIGHTBLUE_EX + "[👀] Expected:\t" + Style.BRIGHT + str(args.expected), Style.RESET_ALL)
