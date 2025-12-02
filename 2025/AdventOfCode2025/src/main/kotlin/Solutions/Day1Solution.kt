f35635778d2eca81aaf2371c5fe730a85ba4a44e
Richard Lim <hello@rclim95.dev>
2025-12-01 20:59:55 -0600

    [2025 - Day 1] Implement Part 2 of Day 1
    This one was quick tricky!
    
    But nothing like implementing your own "modulo" and counting the number
    of times your dial hits zero (and making sure you're not doing any silly
    off-by-one errors :sweat_smile:) will fix everything!

--- 2025/AdventOfCode2025/src/main/kotlin/Solutions/Day1Solution.kt
+++ 2025/AdventOfCode2025/src/main/kotlin/Solutions/Day1Solution.kt
@@ -2,6 +2,7 @@ package solutions
 
 import DaySolution
 import java.util.Scanner
+import kotlin.math.abs
 
 /**
  * Solves Day 1 of Advent of Code 2025.
@@ -50,6 +51,71 @@ object Day1Solution : DaySolution {
      * @return The solution to Part 2, based on the provided input.
      */
     override fun runPart2(input: Scanner): String {
-        TODO("Not yet implemented.")
+        var password: Int = 0
+        var dialPosition: Int = 50;
+
+        // Each line should correspond to whether we're turning this 100-tick knob to the left ("L")
+        // or right ("R").
+        while (input.hasNext()) {
+            val currentRotation: String = input.next()
+            var rotationTicks: Int = currentRotation.substring(1).toInt()
+
+            // For Part 2, it seems that the password method for this problem is based on the number of times
+            // the dial has reached zero while rotating--not just when it stopped at zero.
+            //
+            // We'll need to take that into account. Figure out if we'll be "over" 100 or "under" 0 and implement our
+            // own for of "modulo 100" to handle this.
+            while (rotationTicks > 0) {
+                if (currentRotation.startsWith("L")) {
+                    if (dialPosition - rotationTicks < 0) {
+                        // We're going to be wrapping around from 0 to 100, so adjust our dialPosition to go to
+                        // 100 and update our rotationTicks with the remaining rotations we need to do leftward.
+                        //
+                        // Note that we don't want to increment our password if dialPosition is 0 (the previous
+                        // rotation step should've counted it).
+                        if (dialPosition != 0) {
+                            password++
+                            rotationTicks -= dialPosition
+                            System.err.println("During $currentRotation, dialPosition rotated to 0. ${rotationTicks} ticks remain.")
+                        }
+
+                        dialPosition = 100
+                    }
+                    else {
+                        // No wrap around occurred, rotate left (subtract) as-is.
+                        dialPosition -= rotationTicks
+                        rotationTicks = 0
+
+                        // If our dialPosition is zero after applying our rotation, make sure to increment our
+                        // password!
+                        if (dialPosition == 0) {
+                            password++
+                        }
+
+                        System.err.println("After $currentRotation, dialPosition is now $dialPosition.")
+                    }
+                }
+                else if (currentRotation.startsWith("R")) {
+                    if (dialPosition + rotationTicks > 99) {
+                        // We're going to be wrapping around from 100 to 0, so adjust our dialPosition to go to 0
+                        // and update our rotationTicks with the remaining rotations we need to do rightward.
+                        password++
+                        rotationTicks -= 100 - dialPosition
+                        dialPosition = 0
+
+                        System.err.println("During $currentRotation, dialPosition rotated to 0. ${rotationTicks - dialPosition} ticks remain.")
+                    }
+                    else {
+                        // No wrap around occurred, rotate right (add) as-is.
+                        dialPosition += rotationTicks
+                        rotationTicks = 0
+
+                        System.err.println("After $currentRotation, dialPosition is now $dialPosition.")
+                    }
+                }
+            }
+        }
+
+        return password.toString()
     }
 }
