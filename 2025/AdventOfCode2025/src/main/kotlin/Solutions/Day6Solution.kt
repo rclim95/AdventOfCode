08e43205ecb781a3da92cfddfc1f24d3a5c407e3
Richard Lim <hello@rclim95.dev>
2025-12-08 20:56:40 -0600

    [2025 - Day 6] Implement Part 2 of Day 6
    ...and we're at the half-way mark for this year's Advent of Code! 🥳
    
    Day 6's Part 2 is also not bad. Rather than trying to get optimal with
    some math formulas (I'm sure there is a way to build the digits we need
    to add or multiply in a mathematical way instead of doing simple string
    concatenation), do the following:
    
    1. Read each line into a list.
    2. Go through each column of character in each string that is in the
       list and build the number. Store this in a list.
    3. Keep track of the current operation we're doing (add or multiply) by
       looking at the last line.
    4. When we come across a new operator, add or multiply the number that's
       been stored in the list and move on to the next one.
    
    Ta-da! 🎉

--- 2025/AdventOfCode2025/src/main/kotlin/Solutions/Day6Solution.kt
+++ 2025/AdventOfCode2025/src/main/kotlin/Solutions/Day6Solution.kt
@@ -2,6 +2,8 @@ package solutions
 
 import DaySolution
 import java.util.Scanner
+import java.util.Stack
+import kotlin.math.log10
 
 /**
  * Solves Day 6 of Advent of Code 2025.
@@ -64,6 +66,83 @@ object Day6Solution : DaySolution {
     }
 
     override fun runPart2(input: Scanner): String {
-        TODO()
+        // The input should be a column of numbers on each line. The very last line will contain the operation that we
+        // want to perform on all those numbers.
+        //
+        // Part 2 has a twist though: cephalopods apparently read numbers from right-to-left, column-by-column. So if
+        // you see something like:
+        //
+        // 123 456
+        //   1 92
+        // +   *
+        //
+        // This is really:
+        // - Column 1: 31 + 2  + 1  = 34
+        // - Column 2: 6  * 52 * 49 = 15288
+        //
+        // Note that spaces do matter here (e.g., the position of 1 vs. 92 on the second row).
+        //
+        // Because of this, read all lines that are in our input file and index each character in each string to figure
+        // out the number to add or multiply. And to take care of spacing issues of each math problem ("how do I know if
+        // this is a new math problem column?"), use the last row containing the operators to help us (the operators are
+        // aligned to the "start" of each math problem column).
+        val lines = buildList() {
+            while (input.hasNextLine()) {
+                add(input.nextLine())
+            }
+        }
+
+        var runningSum = 0L
+        var currentOperator: Char? = null
+        val numbersToAggregate = mutableListOf<Long>()
+        val longestLine = lines.maxOf { l -> l.length } // Get around silly trimming issues. :)
+        val operationLine = lines.last()
+        for (i in 0..<longestLine) {
+            if (i < operationLine.length && (operationLine[i] == '*' || operationLine[i] == '+')) {
+                if (currentOperator != null) {
+                    // We have come across an operation. Aggregate the numbers in our list and apply the operation requested.
+                    val answer = when (currentOperator) {
+                        '*' -> numbersToAggregate.reduce { acc, n -> acc * n }
+                        '+' -> numbersToAggregate.reduce { acc, n -> acc + n }
+                        else -> error("Unsupported operator: $currentOperator")
+                    }
+
+                    System.err.print(numbersToAggregate.joinToString(separator = " $currentOperator ", postfix = " = "))
+                    System.err.println(answer)
+                    numbersToAggregate.clear()
+                    runningSum += answer
+                }
+
+                // Now read in the current operator.
+                currentOperator = operationLine[i]
+            }
+
+            // Read in the current number from top to bottom. To make this easy, read it as a series of string concatenation.
+            var currentNumber = ""
+            for (line in lines.take(lines.size - 1)) {
+                if (i < line.length) {
+                    currentNumber += line[i]
+                }
+            }
+            currentNumber = currentNumber.trim()
+
+            // Make sure currentNumber isn't empty (this can happen if we're about to approach the next math problem column).
+            if (!currentNumber.isEmpty()) {
+                numbersToAggregate.add(currentNumber.toLong())
+            }
+        }
+
+        // Don't forget to aggregate the last column!
+        val answer = when (currentOperator) {
+            '*' -> numbersToAggregate.reduce { acc, n -> acc * n }
+            '+' -> numbersToAggregate.reduce { acc, n -> acc + n }
+            else -> error("Unsupported operator: $currentOperator")
+        }
+
+        System.err.print(numbersToAggregate.joinToString(separator = " $currentOperator ", postfix = " = "))
+        System.err.println(answer)
+        runningSum += answer
+
+        return runningSum.toString()
     }
 }
